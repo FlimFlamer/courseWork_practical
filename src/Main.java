635ba7ed1144018f88a83bc779ab772b687dd2a6
FlimFlamer <dronik61@gmail.com>
2022-12-24 19:47:10 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printSplitString("123456789");
+    }
+    public static void printSplitString (String str){
+        char[] chars = str.toCharArray();
+        int size = str.length();
+        int half;
+        if (size % 2 == 0){
+            half = size / 2;
+        } else {
+            half = size / 2 +1;
+        }
+        char a [] = Arrays.copyOfRange(chars, 0, half);
+        char b [] = Arrays.copyOfRange(chars, half, size);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
